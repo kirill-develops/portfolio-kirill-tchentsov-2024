9b3ba3c9a31f851b668cf51391489cb7e2b00da6
Kirill Tchentsov <kirill.develops@gmail.com>
2024-04-28 15:33:33 -0400

    added custom colors to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,41 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            'gradient-conic':
+               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+         },
+         colors: {
+            drawer: { DEFAULT: '#F2F3F7' },
+            font: {
+               heading: '#999999',
+               DEFAULT: 'rgba(0 0 0/0.7)',
+               placeholder: 'rgba(0 0 0/0.5)',
+            },
+            page: {
+               blue: '#2C98F0',
+               orange: '#EC5453',
+               yellow: '#F9BF3F',
+               purple: '#A84CB8',
+               teal: '#2FA499',
+               navy: '#4054B2',
+               red: '#CC403F',
+            },
+         },
+         fontFamily: {
+            serif: ['var(--font-playfair-display)', 'Georgia', 'serif'],
+            sans: ['var(--font-quicksand)', 'Arial', 'sans-serif'],
+         },
       },
-      fontFamily: {
-        serif: ['var(--font-playfair-display)', 'Georgia', 'serif'],
-        sans: ['var(--font-quicksand)', 'Arial', 'sans-serif']
-      }
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
 export default config;
