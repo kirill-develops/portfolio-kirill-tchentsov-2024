489910ca55d0a8239ec2b76cfd815f34255cc094
Kirill Tchentsov <kirill.develops@gmail.com>
2024-05-04 18:57:04 -0400

    corrected spacing in text issue

--- src/app/ui/Sections/AboutSection.tsx
+++ src/app/ui/Sections/AboutSection.tsx
@@ -14,18 +14,18 @@ export default function AboutSection() {
          </HeadingsFrame>
          <Body>
             <BodyOpener>Hi I&apos;m Kirill Tchentsov </BodyOpener>I am a web
-            developer specializing in
-            <Body black>JavaScript</Body>, <Body black>React</Body> (including{' '}
-            <Body black>Context API</Body>, building{' '}
-            <Body black>custom hooks</Body> and <Body black>Redux</Body> for
-            state management), <Body black>Next.js</Body>,{' '}
-            <Body black>Node.js</Body> and <Body black>API construction</Body>{' '}
-            to create responsive and engaging digital dashboards that drive
-            customer engagement and revenue growth. Additionally, I have
-            experience with <Body black>AWS</Body> and <Body black>Git</Body>,
-            allowing me to optimize workflows and ensure efficient project
-            delivery. I&apos;m enthusiastic about test-driven development
-            principles through frameworks like <Body black>Jest</Body> and{' '}
+            developer specializing in <Body black>JavaScript</Body>,{' '}
+            <Body black>React</Body> (including <Body black>Context API</Body>,
+            building <Body black>custom hooks</Body> and{' '}
+            <Body black>Redux</Body> for state management),{' '}
+            <Body black>Next.js</Body>, <Body black>Node.js</Body> and{' '}
+            <Body black>API construction</Body> to create responsive and
+            engaging digital dashboards that drive customer engagement and
+            revenue growth. Additionally, I have experience with{' '}
+            <Body black>AWS</Body> and <Body black>Git</Body>, allowing me to
+            optimize workflows and ensure efficient project delivery. I&apos;m
+            enthusiastic about test-driven development principles through
+            frameworks like <Body black>Jest</Body> and{' '}
             <Body black>Cyprus</Body>, relational & noSQL databases (
             <Body black>MySQL</Body> and <Body black>MongoDB</Body>),
             authentication using <Body black>Oauth2</Body> and{' '}
