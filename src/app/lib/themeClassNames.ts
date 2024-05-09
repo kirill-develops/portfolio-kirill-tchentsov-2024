846a9db867c0dd355a4806df18f8c37e8b1f104e
Kirill Tchentsov <kirill.develops@gmail.com>
2024-05-09 08:48:45 -0400

    added border color classname utility array

---
+++ src/app/lib/themeClassNames.ts
@@ -0,0 +1,11 @@
+const borderColorsArr = [
+   'border-page-blue',
+   'border-page-orange',
+   'border-page-yellow',
+   'border-page-purple',
+   'border-page-teal',
+   'border-page-navy',
+   'border-page-red',
+];
+
+export default borderColorsArr;
